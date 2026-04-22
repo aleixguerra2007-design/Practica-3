3a34193ebccf98505e9e210243714f01e44a6c48
Aleix Gutiérrez <aleixguerra2007@gmail.com>
2026-04-22 18:06:13 +0200

    Nopuedomas x2

--- src/prog2/model/LlistaExemplars.java
+++ src/prog2/model/LlistaExemplars.java
@@ -13,6 +13,11 @@ public class LlistaExemplars extends Llista<Exemplar>{
         super();
     }
 
+    /**
+     * Aquest mètode afegeix un exemplar a la llista i llença excepció si ja està registrat
+     * @param e
+     * @throws BiblioException si l'exemplar ja està registrat
+     */
     @Override
     public void afegir(Exemplar e) throws BiblioException {
         //Comprobamos que no haya ningún ejemplar registrado con el mismo identificador
@@ -29,4 +34,48 @@ public class LlistaExemplars extends Llista<Exemplar>{
         //Si no hay ningún ejemplar con el mismo identificador, lo añadimos a la lista:
         llista.add(e);
     }
+
+    /**
+     * Aquest mètode esborra un exemplar de la llista
+     * @param e
+     * @throws BiblioException si no es troba l'exemplar en la llista
+     */
+    public void esborrar(Exemplar e) throws BiblioException{
+        boolean encontrado = false;
+        String idReferencia = e.getId();
+        //Buscamos el elemento que queremos eliminar de la lista:
+        Iterator<Exemplar> it = llista.iterator();
+        Exemplar exemplar;
+        while(it.hasNext()){
+            exemplar = it.next();
+            if(exemplar.getId().equals(idReferencia)){
+                encontrado = true;
+                it.remove();
+            }
+        }
+        //Lanzamos excepción si no se ha encontrado el elemento indicado:
+        if(!encontrado){
+            throw new BiblioException("Aquest exemplar no està registrat");
+        }
+    }
+
+    /**
+     * Aquest mètode indica si la llista conté un exemplar amb un identificador concret.
+     * @param identificador
+     * @return true si està, false altrament.
+     */
+    public boolean contains(String identificador){
+        //Buscamos en la lista si hay un ejemplar con el identificador indicado.
+        Exemplar exemplar;
+        Iterator<Exemplar> it = llista.iterator();
+        while(it.hasNext()){
+            exemplar = it.next();
+            if(exemplar.getId().equals(identificador)){
+                return true;
+            }
+        }
+        //Si no lo encontramos, devolvemos false
+        return false;
+    }
+
 }
--- src/prog2/model/LlistaUsuaris.java
+++ src/prog2/model/LlistaUsuaris.java
@@ -1,7 +1,73 @@
 package prog2.model;
 
+import prog2.vista.BiblioException;
+import java.util.Iterator;
 public class LlistaUsuaris extends Llista<Usuari>{
     public LlistaUsuaris(){
         super();
+    }
+
+    /**
+     * Aquest mètode afegeix un usuari a la llista
+     * @param u
+     * @throws BiblioException si l'usuari ja està registrat
+     */
+    @Override
+    public void afegir(Usuari u) throws BiblioException{
+        String emailReferencia = u.getEmail();
+        //Comprobamos que el usuario no está registrado ya:
+        Iterator<Usuari> it = llista.iterator();
+        Usuari usuari;
+        while(it.hasNext()){
+            usuari = it.next();
+            if(usuari.getEmail().equals(emailReferencia)) {
+                throw new BiblioException("Aquest usuari ja està registrat");
+            }
+        }
+        //Añadimos el usuario a la lista:
+        llista.add(u);
+    }
+
+    /**
+     * Aquest mètode esborra un usuari de la llista
+     * @param u
+     * @throws BiblioException si l'usuari indicat no està en la llista
+     */
+    public void esborrar(Usuari u) throws BiblioException{
+        boolean encontrado = false;
+        String emailReferencia = u.getEmail();
+        //Buscamos el usuario en la lista
+        Iterator<Usuari> it = llista.iterator();
+        Usuari usuari;
+        while(it.hasNext()){
+            usuari = it.next();
+            if(usuari.getEmail().equals(emailReferencia)){
+                encontrado = true;
+                it.remove();
+            }
+        }
+        //Lanzamos excepción si no lo hemos encontrado en la lista:
+        if(!encontrado){
+            throw new BiblioException("L'usuari indicat no està registrat");
+        }
+    }
+
+    /**
+     * Aquest mètode indica si un usuari amb un email concret està en la llista
+     * @param emailRef
+     * @return true si està, false altrament.
+     */
+    public boolean contains(String emailRef){
+        //Recorremos la lista de elementos para ver si está el usuario con el correo indicado:
+        Iterator<Usuari> it = llista.iterator();
+        Usuari usuari;
+        while(it.hasNext()){
+            usuari = it.next();
+            if(usuari.getEmail().equals(emailRef)){
+                return true;
+            }
+        }
+        //Devolevmos false si no lo encontramos
+        return false;
     }
 }
